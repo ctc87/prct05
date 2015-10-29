8f75b56145c53393565a82deedee6deee730375c
ctc87 <carlos.troyano.carmona@gmail.com>
2015-10-29 13:35:10 +0000

    Segundo commit con los test(corregido)

---
+++ test/tc_fraction.rb
@@ -0,0 +1,26 @@
+require '../lib/fraction'
+require 'test/unit'
+
+#puts "Start test fraction \nNeeds develop the simplify part"
+
+class TestFraction < Test::Unit::TestCase
+	def setup
+		@unit = Fraction.new(1,1)
+		@twoHalfs = Fraction.new(2,2)
+		@oneCuarter = Fraction.new(1,4)	
+	end
+
+	def test_simple
+		assert_equal("1/1", @unit.to_S)
+		assert_equal("2/2", @twoHalfs.to_S)
+		assert_equal("1/4", @oneCuarter.to_S)
+		assert_equal("5/1", (@unit * 5).to_S)
+		assert_equal("1/5", (@unit / 5).to_S)
+		assert_equal("4/4", (@twoHalfs * @twoHalfs).to_S)
+		assert_equal("10/8", (@twoHalfs + @oneCuarter).to_S)
+		assert_equal("6/8", (@twoHalfs - @oneCuarter).to_S)
+	end
+	def test_type_check
+    		assert_raise(RuntimeError) {Fraction.new('1','1')}
+  	end
+end
